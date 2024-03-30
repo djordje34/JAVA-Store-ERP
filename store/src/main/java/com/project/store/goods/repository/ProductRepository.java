49bb97d2d52cc63fd3bb13c2f2326207604ae21f
djordje34 <djordje00karisic@gmail.com>
2024-03-30 15:39:14 +0100

    Create ProductRepository.java

---
+++ store/src/main/java/com/project/store/goods/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.project.store.goods.repository;
+import com.project.store.goods.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    void flush();
+    List<Product> findAll();
+
+    Product save(Product product);
+
+    Optional<Product> findById(Long id);
+
+    void delete(Product product);
+}
