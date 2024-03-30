61835957544e5b63193d75d012e623b3e7b24109
djordje34 <djordje00karisic@gmail.com>
2024-03-30 15:39:08 +0100

    Create InventoryItemRepository.java

---
+++ store/src/main/java/com/project/store/goods/repository/InventoryItemRepository.java
@@ -0,0 +1,16 @@
+package com.project.store.goods.repository;
+
+import com.project.store.goods.entity.InventoryItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryItemRepository extends JpaRepository<InventoryItem, Long> {
+    void flush();
+
+    InventoryItem save(InventoryItem invItem);
+
+    Optional<InventoryItem> findById(Long id);
+
+    void delete(InventoryItem invItem);
+}
