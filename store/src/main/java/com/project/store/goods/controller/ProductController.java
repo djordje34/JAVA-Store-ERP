eaaab8657343169b587389d1942365bd513234ad
djordje34 <djordje00karisic@gmail.com>
2024-03-31 00:12:26 +0100

    Create ProductController.java

---
+++ store/src/main/java/com/project/store/goods/controller/ProductController.java
@@ -0,0 +1,46 @@
+package com.project.store.goods.controller;
+
+import com.project.store.goods.entity.Product;
+import com.project.store.goods.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productService.getProductById(id);
+        return ResponseEntity.ok(product.orElse(null));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+        Product savedProduct = productService.saveProduct(product);
+        return ResponseEntity.ok(savedProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+}
