a2dbe0b3973389a2ed67167083de1602ccd8be40
djordje34 <djordje00karisic@gmail.com>
2024-03-31 00:12:34 +0100

    Create SupplierController.java

---
+++ store/src/main/java/com/project/store/goods/controller/SupplierController.java
@@ -0,0 +1,50 @@
+package com.project.store.goods.controller;
+
+import com.project.store.goods.entity.Supplier;
+import com.project.store.goods.entity.Warehouse;
+import com.project.store.goods.service.SupplierService;
+import com.project.store.goods.service.WarehouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/suppliers")
+public class SupplierController {
+
+    private final SupplierService supplierService;
+
+    @Autowired
+    public SupplierController(SupplierService supplierService){
+        this.supplierService = supplierService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Supplier>> getAllSuppliers() {
+        List<Supplier> suppliers = supplierService.getAllSuppliers();
+        return ResponseEntity.ok(suppliers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Supplier> getSupplierById(@PathVariable Long id) {
+        Optional<Supplier> supplier = supplierService.getSupplierById(id);
+        return ResponseEntity.ok(supplier.orElse(null));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Supplier> saveSupplier(@RequestBody Supplier supplier) {
+        System.out.println(supplier.getSupplierName());
+        Supplier savedSupplier = supplierService.saveSupplier(supplier);
+        return ResponseEntity.ok(savedSupplier);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSupplier(@PathVariable Long id) {
+        supplierService.deleteSupplier(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
