d69ca68215ebb7761ef0f94413a429eec99f7741
djordje34 <djordje00karisic@gmail.com>
2024-03-31 00:12:47 +0100

    Create SupplierService.java

---
+++ store/src/main/java/com/project/store/goods/service/SupplierService.java
@@ -0,0 +1,40 @@
+package com.project.store.goods.service;
+
+import com.project.store.goods.entity.Product;
+import com.project.store.goods.entity.Supplier;
+import com.project.store.goods.repository.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SupplierService {
+
+    private final SupplierRepository supplierRepository;
+
+    @Autowired
+    public SupplierService(SupplierRepository supplierRepository){
+        this.supplierRepository = supplierRepository;
+    }
+
+    public List<Supplier> getAllSuppliers() {
+        return supplierRepository.findAll();
+    }
+
+    public Optional<Supplier> getSupplierById(Long id) {
+        return supplierRepository.findById(id);
+    }
+
+    public Supplier saveSupplier(Supplier supplier) {
+        return supplierRepository.save(supplier);
+    }
+
+    public void deleteSupplier(Long id){
+        Optional<Supplier> supplier = supplierRepository.findById(id);
+
+        supplier.ifPresent(supplierRepository::delete);
+    }
+
+}
