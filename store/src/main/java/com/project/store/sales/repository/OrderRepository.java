959ee08d754943e54e98bef57988c8b2d621c29b
djordje34 <djordje00karisic@gmail.com>
2024-03-30 15:39:39 +0100

    Create OrderRepository.java

---
+++ store/src/main/java/com/project/store/sales/repository/OrderRepository.java
@@ -0,0 +1,15 @@
+package com.project.store.sales.repository;
+import com.project.store.sales.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    void flush();
+
+    Order save(Order order);
+
+    Optional<Order> findById(Long id);
+
+    void delete(Order order);
+}
