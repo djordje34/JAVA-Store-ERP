4ab4ca1b61506b6a55b83db9f5a589cde4a7e5e7
djordje34 <djordje00karisic@gmail.com>
2024-03-30 15:39:23 +0100

    Create SupplierRepository.java

---
+++ store/src/main/java/com/project/store/goods/repository/SupplierRepository.java
@@ -0,0 +1,15 @@
+package com.project.store.goods.repository;
+
+import com.project.store.goods.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+    void flush();
+
+    Supplier save(Supplier supplier);
+    Optional<Supplier> findById(Long id);
+
+    void delete(Supplier supplier);
+}
