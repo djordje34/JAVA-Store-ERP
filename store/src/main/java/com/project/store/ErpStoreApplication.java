3b986f19e7e6eeaf2f138a3595dc95bc060c815d
djordje34 <djordje00karisic@gmail.com>
2024-03-31 00:13:08 +0100

    Create ErpStoreApplication.java

---
+++ store/src/main/java/com/project/store/ErpStoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpStoreApplication.class, args);
+	}
+
+}
