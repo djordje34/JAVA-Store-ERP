ab3f7e40e75689e4c4279dbac0428363e5bbf679
djordje34 <djordje00karisic@gmail.com>
2024-04-03 19:42:58 +0200

    Create OrderItemService.java

---
+++ store/src/main/java/com/project/store/sales/service/OrderItemService.java
@@ -0,0 +1,36 @@
+package com.project.store.sales.service;
+
+import com.project.store.sales.entity.OrderItem;
+import com.project.store.sales.repository.OrderItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderItemService {
+    private final OrderItemRepository orderItemRepository;
+
+    @Autowired
+    public OrderItemService(OrderItemRepository orderItemRepository){
+        this.orderItemRepository = orderItemRepository;
+    }
+
+    public List<OrderItem> getAllOrderItems() {
+        return orderItemRepository.findAll();
+    }
+
+    public Optional<OrderItem> getOrderItemById(Long id) {
+        return orderItemRepository.findById(id);
+    }
+
+    public OrderItem saveOrderItem(OrderItem orderItem) {
+        return orderItemRepository.save(orderItem);
+    }
+
+    public void deleteOrderItem(Long id){
+        Optional<OrderItem> orderItem = orderItemRepository.findById(id);
+        orderItem.ifPresent(orderItemRepository::delete);
+    }
+}
