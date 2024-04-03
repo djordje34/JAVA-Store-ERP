6e3e6bc83a6eceb200f06934a9be11d4f7f404fa
djordje34 <djordje00karisic@gmail.com>
2024-04-03 19:42:50 +0200

    Create CustomerRepository.java

---
+++ store/src/main/java/com/project/store/sales/repository/CustomerRepository.java
@@ -0,0 +1,21 @@
+package com.project.store.sales.repository;
+
+import com.project.store.goods.entity.Product;
+import com.project.store.sales.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    void flush();
+    List<Customer> findAll();
+
+    Customer save(Customer customer);
+
+    Optional<Customer> findById(Long id);
+
+    void delete(Customer customer);
+}
