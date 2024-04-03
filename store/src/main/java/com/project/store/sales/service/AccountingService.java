9cf1510810435ecb981a5008ef1df5794fccd434
djordje34 <djordje00karisic@gmail.com>
2024-04-03 19:42:52 +0200

    Create AccountingService.java

---
+++ store/src/main/java/com/project/store/sales/service/AccountingService.java
@@ -0,0 +1,37 @@
+package com.project.store.sales.service;
+
+import com.project.store.sales.entity.Accounting;
+import com.project.store.sales.entity.Invoice;
+import com.project.store.sales.repository.AccountingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AccountingService {
+    private final AccountingRepository accountingRepository;
+
+    @Autowired
+    public AccountingService(AccountingRepository accountingRepository){
+        this.accountingRepository = accountingRepository;
+    }
+
+    public List<Accounting> getAllAccountings(){
+        return accountingRepository.findAll();
+    }
+
+    public Optional<Accounting> findAccountingById(Long id){
+        return accountingRepository.findById(id);
+    }
+
+    public Accounting saveAccounting(Accounting accounting){
+        return accountingRepository.save(accounting);
+    }
+
+    public void deleteAccounting(Long id){
+        Optional<Accounting> accounting = accountingRepository.findById(id);
+        accounting.ifPresent(accountingRepository::delete);
+    }
+}
