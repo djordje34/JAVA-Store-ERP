6237b74e4b3ff69506529b818f23a7508ef08e0c
djordje34 <djordje00karisic@gmail.com>
2024-04-03 19:42:41 +0200

    Create ReservationController.java

---
+++ store/src/main/java/com/project/store/goods/controller/ReservationController.java
@@ -0,0 +1,48 @@
+package com.project.store.goods.controller;
+
+import com.project.store.goods.entity.Reservation;
+import com.project.store.goods.entity.Warehouse;
+import com.project.store.goods.service.ReservationService;
+import com.project.store.goods.service.WarehouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/reservations")
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    @Autowired
+    public ReservationController(ReservationService reservationService){
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Reservation>> getAllReservations() {
+        List<Reservation> reservations = reservationService.getAllReservations();
+        return ResponseEntity.ok(reservations);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reservation> getReservationById(@PathVariable Long id) {
+        Optional<Reservation> reservation = reservationService.getReservationById(id);
+        return ResponseEntity.ok(reservation.orElse(null));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Reservation> saveReservation(@RequestBody Reservation reservation) {
+        Reservation savedReservation = reservationService.saveReservation(reservation);
+        return ResponseEntity.ok(savedReservation);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+        reservationService.deleteReservation(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
