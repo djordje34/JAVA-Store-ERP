bc8bd66d8187f0c963af92fa6d17a70d62cc2204
djordje34 <djordje00karisic@gmail.com>
2024-04-03 19:42:54 +0200

    Create CustomerService.java

---
+++ store/src/main/java/com/project/store/sales/service/CustomerService.java
@@ -0,0 +1,37 @@
+package com.project.store.sales.service;
+
+import com.project.store.goods.entity.Product;
+import com.project.store.sales.entity.Customer;
+import com.project.store.sales.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCustomerById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public void deleteCustomer(Long id){
+        Optional<Customer> customer = customerRepository.findById(id);
+        customer.ifPresent(customerRepository::delete);
+    }
+}
