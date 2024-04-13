f77ed01766c2e76a64ea30e69122fb53e4be5a8e
djordje34 <djordje00karisic@gmail.com>
2024-04-13 11:17:11 +0200

    Create InvoiceController.java

---
+++ store/src/main/java/com/project/store/sales/controller/InvoiceController.java
@@ -0,0 +1,47 @@
+package com.project.store.sales.controller;
+
+import com.project.store.sales.entity.Invoice;
+import com.project.store.sales.service.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/invoices")
+public class InvoiceController {
+
+    private final InvoiceService invoiceService;
+
+    @Autowired
+    public InvoiceController(InvoiceService invoiceService){
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Invoice>> getAllInvoices() {
+        List<Invoice> invoices = invoiceService.getAllInvoices();
+        return ResponseEntity.ok(invoices);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Invoice> getInvoiceById(@PathVariable Long id) {
+        Optional<Invoice> invoice = invoiceService.getInvoiceById(id);
+        return ResponseEntity.ok(invoice.orElse(null));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Invoice> saveInvoice(@RequestBody Invoice invoice) {
+        Invoice savedInvoice = invoiceService.saveInvoice(invoice);
+        return ResponseEntity.ok(savedInvoice);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteInvoice(@PathVariable Long id) {
+        invoiceService.deleteInvoice(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
