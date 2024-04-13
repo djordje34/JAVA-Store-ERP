fb7e05b6562ff6f54b66966af2d50e19bc3bc02c
djordje34 <djordje00karisic@gmail.com>
2024-04-13 11:17:13 +0200

    Create OrderController.java

---
+++ store/src/main/java/com/project/store/sales/controller/OrderController.java
@@ -0,0 +1,46 @@
+package com.project.store.sales.controller;
+
+import com.project.store.sales.entity.Order;
+import com.project.store.sales.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService){
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Order>> getAllOrders() {
+        List<Order> orders = orderService.getAllOrders();
+        return ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
+        Optional<Order> order = orderService.getOrderById(id);
+        return ResponseEntity.ok(order.orElse(null));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Order> saveOrder(@RequestBody Order order) {
+        Order savedOrder = orderService.saveOrder(order);
+        return ResponseEntity.ok(savedOrder);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+        return ResponseEntity.noContent().build();
+    }
+}
