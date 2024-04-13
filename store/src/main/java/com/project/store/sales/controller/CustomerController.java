a797e3b0dd713ba322c68cd681f4aa1e76438bf1
djordje34 <djordje00karisic@gmail.com>
2024-04-13 11:17:09 +0200

    Create CustomerController.java

---
+++ store/src/main/java/com/project/store/sales/controller/CustomerController.java
@@ -0,0 +1,46 @@
+package com.project.store.sales.controller;
+
+import com.project.store.sales.entity.Customer;
+import com.project.store.sales.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> customers = customerService.getAllCustomers();
+        return ResponseEntity.ok(customers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
+        Optional<Customer> customer = customerService.getCustomerById(id);
+        return ResponseEntity.ok(customer.orElse(null));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Customer> saveCustomer(@RequestBody Customer customer) {
+        Customer savedCustomer = customerService.saveCustomer(customer);
+        return ResponseEntity.ok(savedCustomer);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+        return ResponseEntity.noContent().build();
+    }
+}
