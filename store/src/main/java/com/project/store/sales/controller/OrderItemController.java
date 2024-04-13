dfa01ef79c8ea77637e32a5ad7de8226628d4707
djordje34 <djordje00karisic@gmail.com>
2024-04-13 11:17:16 +0200

    Create OrderItemController.java

---
+++ store/src/main/java/com/project/store/sales/controller/OrderItemController.java
@@ -0,0 +1,46 @@
+package com.project.store.sales.controller;
+
+import com.project.store.sales.entity.Invoice;
+import com.project.store.sales.entity.OrderItem;
+import com.project.store.sales.service.OrderItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/orderItems")
+public class OrderItemController {
+    private final OrderItemService orderItemService;
+
+    @Autowired
+    public OrderItemController(OrderItemService orderItemService){
+        this.orderItemService = orderItemService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<OrderItem>> getAllOrderItems() {
+        List<OrderItem> orderItems = orderItemService.getAllOrderItems();
+        return ResponseEntity.ok(orderItems);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderItem> getOrderItemById(@PathVariable Long id) {
+        Optional<OrderItem> orderItem = orderItemService.getOrderItemById(id);
+        return ResponseEntity.ok(orderItem.orElse(null));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<OrderItem> saveOrderItem(@RequestBody OrderItem orderItem) {
+        OrderItem savedOrderItem = orderItemService.saveOrderItem(orderItem);
+        return ResponseEntity.ok(savedOrderItem);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrderItem(@PathVariable Long id) {
+        orderItemService.deleteOrderItem(id);
+        return ResponseEntity.noContent().build();
+    }
+}
