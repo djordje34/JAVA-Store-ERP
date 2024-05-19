d448910c2be65314de73d86f8de868e5a9115a45
djordje34 <djordje00karisic@gmail.com>
2024-05-20 00:06:25 +0200

    Update ProductService.java

--- store/src/main/java/com/project/store/goods/service/ProductService.java
+++ store/src/main/java/com/project/store/goods/service/ProductService.java
@@ -2,6 +2,9 @@ package com.project.store.goods.service;
 
 import com.project.store.goods.entity.Product;
 import com.project.store.goods.repository.ProductRepository;
+import com.project.store.messaging.config.RabbitMQConfigurator;
+import com.project.store.messaging.events.ProductEvent;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +15,11 @@ import java.util.Optional;
 public class ProductService {
 
     private final ProductRepository productRepository;
-
+    private final RabbitTemplate rabbitTemplate;
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, RabbitTemplate rabbitTemplate) {
         this.productRepository = productRepository;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     public List<Product> getAllProducts() {
@@ -27,7 +31,11 @@ public class ProductService {
     }
 
     public Product saveProduct(Product product) {
-        return productRepository.save(product);
+        Product savedProduct = productRepository.save(product);
+        ProductEvent productEvent = ProductEvent.createNewProductEvent(product);
+        System.out.println(savedProduct);
+        rabbitTemplate.convertAndSend(RabbitMQConfigurator.PRODUCT_TOPIC_EXCHANGE, "products.created", productEvent);
+        return savedProduct;
     }
 
     public void deleteProduct(Long id){
