6e592bb4fb3460e61d2f49d1bfacd09e2a78e519
djordje34 <djordje00karisic@gmail.com>
2024-05-26 16:55:08 +0200

    Update ProductService.java

--- store/src/main/java/com/project/store/goods/service/ProductService.java
+++ store/src/main/java/com/project/store/goods/service/ProductService.java
@@ -4,9 +4,11 @@ import com.project.store.goods.entity.Product;
 import com.project.store.goods.repository.ProductRepository;
 import com.project.store.messaging.config.RabbitMQConfigurator;
 import com.project.store.messaging.events.ProductEvent;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -38,9 +40,35 @@ public class ProductService {
         return savedProduct;
     }
 
-    public void deleteProduct(Long id){
-        Optional<Product> product = productRepository.findById(id);
+    public void deleteProduct(Long id) {
+        Optional<Product> productOptional = productRepository.findById(id);
 
-        product.ifPresent(productRepository::delete);
+        if (productOptional.isEmpty()) {
+            throw new EntityNotFoundException("Product not found with id: " + id);
+        }
+        Product product = productOptional.get();
+        productRepository.delete(product);
+        ProductEvent productEvent = ProductEvent.createDeletedProductEvent(product);
+        rabbitTemplate.convertAndSend(RabbitMQConfigurator.PRODUCT_TOPIC_EXCHANGE, "products.deleted", productEvent);
     }
+
+    @Transactional
+    public Product updateProduct(Long id, Product updatedProduct) {
+        Optional<Product> existingProductOptional = productRepository.findById(id);
+        if (existingProductOptional.isEmpty()) {
+            throw new EntityNotFoundException("Product not found with id: " + id);
+        }
+        Product existingProduct = existingProductOptional.get();
+
+        existingProduct.setProductName(updatedProduct.getProductName());
+        existingProduct.setUnitOfMeasure(updatedProduct.getUnitOfMeasure());
+
+        Product savedProduct = productRepository.save(existingProduct);
+        ProductEvent productEvent = ProductEvent.createUpdatedProductEvent(savedProduct);
+        System.out.println(savedProduct);
+        rabbitTemplate.convertAndSend(RabbitMQConfigurator.PRODUCT_TOPIC_EXCHANGE, "products.updated", productEvent);
+        return savedProduct;
+
+    }
+
 }
