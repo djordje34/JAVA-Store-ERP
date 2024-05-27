f6d634011454f1d1a9a5a2c47f9748be5c2e6cc3
djordje34 <djordje00karisic@gmail.com>
2024-05-27 21:18:14 +0200

    Update ProductListener.java

--- store/src/main/java/com/project/store/sales/listeners/ProductListener.java
+++ store/src/main/java/com/project/store/sales/listeners/ProductListener.java
@@ -13,12 +13,17 @@ public class ProductListener implements Serializable {
 
         ProductEvent.EventType currEvent = productEvent.getEventType();
         switch (currEvent) {
+            case NONE -> {
+            }
             case NEW_PRODUCT ->
                     System.out.println(currEvent + " - NEW PRODUCT AVAILABLE ON THE MARKET - " + productEvent.getProduct().toString());
             case UPDATED_PRODUCT ->
                     System.out.println(currEvent + " - UPDATED PRODUCT ON THE MARKET -" + productEvent.getProduct().toString());
             case DELETED_PRODUCT ->
-                    System.out.println(currEvent + " - REMOVED PRODUCT ON THE MARKET -" + productEvent.getProduct().toString());
+                    System.out.println(currEvent + " - REMOVED PRODUCT FROM THE MARKET -" + productEvent.getProduct().toString());
+            case AVAILABLE_PRODUCT -> {
+                System.out.println(currEvent + " - REMOVED PRODUCT IN THE INVENTORY -" + productEvent.getProduct().toString());
+            }
             default -> throw new IllegalStateException("Unexpected value: " + currEvent);
         }
     }
