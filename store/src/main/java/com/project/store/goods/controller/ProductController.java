7b8514b1a0069bff3af90fefad2d69ab0ecebfbb
djordje34 <djordje00karisic@gmail.com>
2024-05-30 17:07:48 +0200

    Update ProductController.java

--- store/src/main/java/com/project/store/goods/controller/ProductController.java
+++ store/src/main/java/com/project/store/goods/controller/ProductController.java
@@ -22,11 +22,10 @@ import java.util.stream.Collectors;
 public class ProductController {
 
     private final ProductService productService;
-    private final SupplierService supplierService;
+
     @Autowired
     public ProductController(ProductService productService, SupplierService supplierService) {
         this.productService = productService;
-        this.supplierService = supplierService;
     }
 
     @GetMapping("/")
@@ -61,49 +60,6 @@ public class ProductController {
         } catch (EntityNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
-    }
-
-    @PostMapping("/receptionOfProducts")
-    public ResponseEntity<?> updateInventory(@RequestBody Map<String, Object> requestBody) {
-        Long supplierId = ((Integer) requestBody.get("supplierId")).longValue();
-        List<Map<String, Object>> inventoryItemMaps = (List<Map<String, Object>>) requestBody.get("inventoryItems");
-
-        Optional<Supplier> existingSupplier = supplierService.getSupplierById(supplierId);
-
-        if (existingSupplier.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Supplier not found");
-        }
-
-        for (Map<String, Object> inventoryItemMap : inventoryItemMaps) {
-            Map<String, Object> productMap = (Map<String, Object>) inventoryItemMap.get("product");
-            Long productId = ((Integer) productMap.get("id")).longValue();
-
-            if (productService.getProductById(productId).isEmpty()) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Product with ID " + productId + " does not exist");
-            }
-        }
-
-        List<InventoryItem> inventoryItems = convertToInventoryItems(inventoryItemMaps);
-        productService.receptionOfProducts(existingSupplier.get(), inventoryItems);
-        return ResponseEntity.ok(inventoryItems);
-    }
-
-    private List<InventoryItem> convertToInventoryItems(List<Map<String, Object>> inventoryItemMaps) {
-        List<InventoryItem> list = new ArrayList<>();
-        for (Map<String, Object> inventoryItemMap : inventoryItemMaps) {
-            InventoryItem inventoryItem = new InventoryItem();
-            inventoryItem.setPurchasePrice((Double) inventoryItemMap.get("purchasePrice"));
-
-            Map<String, Object> productMap = (Map<String, Object>) inventoryItemMap.get("product");
-            Product product = new Product();
-            product.setId(((Integer) productMap.get("id")).longValue());
-            product.setProductName((String) productMap.get("productName"));
-            product.setUnitOfMeasure((String) productMap.get("unitOfMeasure"));
-
-            inventoryItem.setProduct(product);
-            list.add(inventoryItem);
-        }
-        return list;
     }
 
 }
