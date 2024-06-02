637bbbd0eec0dad2fb48eec8744da8586aa405d4
djordje34 <djordje00karisic@gmail.com>
2024-06-03 01:10:10 +0200

    Update AccountingRepository.java

--- store/src/main/java/com/project/store/sales/repository/AccountingRepository.java
+++ store/src/main/java/com/project/store/sales/repository/AccountingRepository.java
@@ -2,7 +2,11 @@ package com.project.store.sales.repository;
 
 import com.project.store.sales.entity.Accounting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountingRepository extends JpaRepository<Accounting, Long> {
@@ -13,4 +17,8 @@ public interface AccountingRepository extends JpaRepository<Accounting, Long> {
     Optional<Accounting> findById(Long id);
 
     void delete(Accounting accounting);
+
+    // 0-pending, 1-successful i 2-cancelled
+    @Query("SELECT a FROM Accounting a WHERE a.dueDate < :now AND a.status = 0")
+    List<Accounting> findExpiredAccountings(LocalDate now);
 }
