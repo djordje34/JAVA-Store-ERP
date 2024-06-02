768ab2f951e42b87f23f34fe987aee494e145d74
djordje34 <djordje00karisic@gmail.com>
2024-06-02 16:37:31 +0200

    Create AdvancedGoodsService.java

---
+++ store/src/main/java/com/project/store/goods/service/AdvancedGoodsService.java
@@ -0,0 +1,87 @@
+package com.project.store.goods.service;
+
+import com.project.store.goods.entity.InventoryItem;
+import com.project.store.goods.entity.Product;
+import com.project.store.goods.entity.Supplier;
+import com.project.store.goods.entity.Warehouse;
+import com.project.store.goods.repository.InventoryItemRepository;
+import com.project.store.goods.repository.ProductRepository;
+import com.project.store.goods.repository.WarehouseRepository;
+import com.project.store.messaging.config.RabbitMQConfigurator;
+import com.project.store.messaging.events.ProductEvent;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class AdvancedGoodsService {
+    private final ProductRepository productRepository;
+    private final InventoryItemRepository inventoryItemRepository;
+    private final WarehouseRepository warehouseRepository;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public AdvancedGoodsService(ProductRepository productRepository, InventoryItemRepository inventoryItemRepository, WarehouseRepository warehouseRepository, RabbitTemplate rabbitTemplate) {
+        this.productRepository = productRepository;
+        this.inventoryItemRepository = inventoryItemRepository;
+        this.warehouseRepository = warehouseRepository;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+    @Transactional
+    public void receptionOfProducts(Supplier supplier, List<InventoryItem> items){ // NIJE TESTIRANO
+        LocalDate curr = LocalDate.now();
+
+        for(InventoryItem item : items){
+            Product product = item.getProduct();
+            Optional<Product> existingProduct = productRepository.findById(product.getId());
+
+            if (existingProduct.isEmpty()) {
+                throw new IllegalArgumentException("Product with ID " + product.getId() + " does not exist");
+            }
+            inventoryItemRepository.save(item);
+
+            Warehouse warehouse = new Warehouse(item, 1, curr, supplier); // quantity nepotreban
+            warehouseRepository.save(warehouse);
+
+            ProductEvent productEvent = ProductEvent.createAvailableProductEvent(product);
+            rabbitTemplate.convertAndSend(RabbitMQConfigurator.PRODUCT_TOPIC_EXCHANGE, "products.available", productEvent);
+        }
+    }
+
+    public boolean getProductState(Long id){
+        List<Warehouse> warehouses = warehouseRepository.findAll();
+
+        for(Warehouse warehouse : warehouses){
+            if(Objects.equals(warehouse.getInventoryItem().getProduct().getId(), id)) return true;
+        }
+        return false;
+    }
+
+    // default impl za formPrice
+    public Double formPrice(Warehouse warehouse){
+        return warehouse.getInventoryItem().getPurchasePrice()* 1.2;
+    }
+    //spec. impl
+    public Double formPrice(Warehouse warehouse, Double factor){
+
+        Optional<List<InventoryItem>> items = inventoryItemRepository.findByProductId(warehouse.getInventoryItem().getProduct().getId());
+
+        if(items.isEmpty()) throw new IllegalArgumentException("There are no items that have product with ID=" + warehouse.getInventoryItem().getProduct().getId() + " ");
+
+        List<InventoryItem> inventoryItems = items.get();
+        Double maxPrice = 0.0;
+        for(InventoryItem item : inventoryItems){
+            maxPrice = inventoryItems.stream()
+                    .mapToDouble(InventoryItem::getPurchasePrice)
+                    .max()
+                    .orElse(0.0);
+        }
+        return maxPrice * factor;
+    }
+}
