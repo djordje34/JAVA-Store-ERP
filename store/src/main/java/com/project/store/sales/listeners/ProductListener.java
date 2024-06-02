013ec20ab42c90fd163bdb6e7c36e81e3e40dca8
djordje34 <djordje00karisic@gmail.com>
2024-06-02 16:38:04 +0200

    Update ProductListener.java

--- store/src/main/java/com/project/store/sales/listeners/ProductListener.java
+++ store/src/main/java/com/project/store/sales/listeners/ProductListener.java
@@ -1,12 +1,33 @@
 package com.project.store.sales.listeners;
 
+import com.project.store.goods.entity.Reservation;
 import com.project.store.messaging.events.ProductEvent;
+import com.project.store.sales.entity.Accounting;
+import com.project.store.sales.entity.OrderItem;
+import com.project.store.sales.service.AccountingService;
+import com.project.store.sales.service.InvoiceService;
+import com.project.store.sales.service.OrderItemService;
+import com.project.store.sales.service.OrderService;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ProductListener implements Serializable {
+    private final OrderService orderService;
+    private final OrderItemService orderItemService;
+    private final InvoiceService invoiceService;
+    private final AccountingService accountingService;
+
+    public ProductListener(OrderService orderService, OrderItemService orderItemService, InvoiceService invoiceService, AccountingService accountingService) {
+        this.orderService = orderService;
+        this.orderItemService = orderItemService;
+        this.invoiceService = invoiceService;
+        this.accountingService = accountingService;
+    }
 
     public void productAction(ProductEvent productEvent){
         System.out.println("SALES: Product Event Occurred");
@@ -21,6 +42,23 @@ public class ProductListener implements Serializable {
                     System.out.println(currEvent + " - UPDATED PRODUCT ON THE MARKET - " + productEvent.getProduct().toString());
             case DELETED_PRODUCT ->
                     System.out.println(currEvent + " - REMOVED PRODUCT FROM THE MARKET - " + productEvent.getProduct().toString());
+            case CHECK_PRODUCT_FAILED -> {
+                orderService.deleteOrder(productEvent.getReservation().getOrder().getId());
+                System.out.println(currEvent + " - ORDER WITH ID = "+ productEvent.getReservation().getOrder().getId() +"FAILED - Not enough quantity of PRODUCT(s) with ID = "+productEvent.getReservation().getProduct().getId() );
+            }
+            case CHECK_PRODUCT_SUCCESSFUL -> {
+            List<Double> prices = productEvent.getPrices();
+            List<Reservation> reservations = productEvent.getReservations();
+
+            List<OrderItem> orderItems = new ArrayList<OrderItem>();
+            for(int i = 0; i< reservations.size(); i++){
+                orderItemService.saveOrderItem(new OrderItem(reservations.get(i).getOrder(), reservations.get(i).getProduct(),reservations.get(i).getQuantity(), prices.get(i)));
+            }
+
+            Accounting accounting = new Accounting(reservations.get(0).getOrder(), prices.stream().reduce(0.0, Double::sum), LocalDate.now().plusDays(3), (byte) 0);
+            accountingService.saveAccounting(accounting);
+                System.out.println(currEvent + " - ORDER SUCCESSFUL - " + accounting.getOrder().toString());
+            }
             case AVAILABLE_PRODUCT -> {
                 System.out.println(currEvent + " - ITEM ADDED TO THE INVENTORY - " + productEvent.getProduct().toString());
             }
