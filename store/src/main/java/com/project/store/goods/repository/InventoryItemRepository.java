4926881b4fbdcdf7adcda55cc05215bad54f7471
djordje34 <djordje00karisic@gmail.com>
2024-06-02 16:37:29 +0200

    Update InventoryItemRepository.java

--- store/src/main/java/com/project/store/goods/repository/InventoryItemRepository.java
+++ store/src/main/java/com/project/store/goods/repository/InventoryItemRepository.java
@@ -3,6 +3,7 @@ package com.project.store.goods.repository;
 import com.project.store.goods.entity.InventoryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryItemRepository extends JpaRepository<InventoryItem, Long> {
@@ -14,5 +15,5 @@ public interface InventoryItemRepository extends JpaRepository<InventoryItem, Lo
 
     void delete(InventoryItem invItem);
 
-    Optional<InventoryItem> findByProductId(Long productId);
+    Optional<List<InventoryItem>> findByProductId(Long productId);
 }
